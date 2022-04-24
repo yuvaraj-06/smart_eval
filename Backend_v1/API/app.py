808329af0d314d97b8368e46bbd20c6e194ce1e7
Yuvaraj Tankala <59510075+yuvaraj-06@users.noreply.github.com>
2022-04-24 15:28:31 -0700

    Update app.py

--- Backend_v1/API/app.py
+++ Backend_v1/API/app.py
@@ -5,36 +5,16 @@ app = FastAPI()
  
 
 @app.get("/")
-def home(request: Request):
-    return {"hello"}
-@app.post("/gr")
-def gr(request: dict):
+def test(request: Request):
+    return {"testing passed"}
+@app.post("/grade")
+def grade(request: dict):
 
 
         evaluator = CannyEval()
         input_json = request 
         report = evaluator.report_card(data_json=input_json)
-        print(report)
+        
         
         
         return report
-#{"1":["Disaster Management is very crucial after what has happened on 21st October 1996.", [["Disaster Management is very important after what has happened on 21st October 1996."], ["Disaster Management is very crucial after what has happened on 22nd October 1996."]]], 
-#"2":["Hitler is a very cruel person. Hitler has tortured many people in Germany.", [["Gandhi is a very cruel person. Gandhi has tortured many people in Germany"], ["Gandhi is a very cruel person. Gandhi has tortured many people in India"] ] ]}
-
-#
-''''
-from fastapi import FastAPI, Request
- 
-app = FastAPI()
-
- 
-
-@app.get("/")
-def home(request: Request):
-    return {"hello"}
-@app.get("/gr")
-def gr(request: Request):
-
-    return {"a":1}
-
-'''
